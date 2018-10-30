d8e03bdc4ae75c35e738d9f3e8c2a307469765e4
Meggan <meggtriplett@gmail.com>
2018-10-30 15:22:42 -0700

    updated app.js to use demo code from class today to create objects without constructors. updated html to contain main content id. updated readme.md to show code reflects that from class demo

--- app.js
+++ app.js
@@ -1,30 +1,285 @@
-var 1standPike = {
-  minCust: 23,  //minimum number of customers in store a hour
-  maxCust: 65,  //maximum number of customer in store a hour
-  avgCookie: 6.3, //average number of cookies bought per customer
-
+var firstAndPike = {
+  storeLocation: '1st and Pike',
+  minCustPerHour: 23,
+  maxCustPerHour: 65,
+  avgCookiesSoldPerCust: 6.3,
+  customersPerHour: [],
+  cookiesPerHour: [],
   hoursOpen: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
-  cookiesNeededByHour: [],
-  // totalCookiesPerHour: [],
-  // totalCookiesPerDay: 0,
-
-  // function used to create a random number of customers based off of the the minCust and maxCust
-  randCust: function (minCust, maxCust){
-    min = Math.ceil(minCust);
-    max = Math.floor(maxCust);
-
-    var randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-    return randomNumber;
+  totalCookiesPerDay: 0,
+  generateRandomCustPerHour: function(min, max) {
+    for(var i = 0; i < this.hoursOpen.length; i++) {
+      var randomCustNumber = Math.floor(Math.random() * (max - min + 1) + min);
+      this.customersPerHour.push(randomCustNumber);
+    }
   },
+  generateHourlySales: function() {
+    // Will generate number of customers per hour using the min/cust and max/cust
+    this.generateRandomCustPerHour(this.minCustPerHour, this.maxCustPerHour);
 
-  totalCookiesPerHour: function (randCust, avgCookie){
-    var cookiesInHour = randCust * avgCookie;
-    return cookiesInHour;
+    // loop to create and push a cookies per hour amount to pair with each hour open
+    for(var i = 0; i < this.hoursOpen.length; i++) {
+      var perHour = Math.round(this.customersPerHour[i] * this.avgCookiesSoldPerCust);
+      this.cookiesPerHour.push(perHour);
+
+      // creates a total amount of cookies for the whole day by adding up each cookies per hour amount
+      this.totalCookiesPerDay += perHour;
+    }
   },
+  render: function() {
+    // Line below will generate hourly sales, which also generates customers per hour
+    this.generateHourlySales();
   
-  hourlyCookies: function (){
-    for (hours = 0; hours < hoursOpen.length; hours++){
-    this.cookiesNeededByHour.push(hourlyCookies)};
+    var mainEl = document.getElementById('main-content');
+    var containerEl = document.createElement('section');
+  
+    var headingEl = document.createElement('h3');
+    headingEl.textContent = this.storeLocation;
+    containerEl.appendChild(headingEl);
+  
+    var ulEl = document.createElement('ul');
+  
+    for(var i = 0; i < this.hoursOpen.length; i++) {
+      var liEl = document.createElement('li');
+      liEl.textContent = `${this.hoursOpen[i]}: ${this.cookiesPerHour[i]} cookies`; // '6am: 23 cookies'
+      ulEl.appendChild(liEl);
+    }
+  
+    var totalEl = document.createElement('li');
+    totalEl.textContent = `Total: ${this.totalCookiesPerDay} cookies`;
+    ulEl.appendChild(totalEl);
+  
+    containerEl.appendChild(ulEl);
+    mainEl.appendChild(containerEl);
   },
-
 };
+
+var seatacAirport = {
+  storeLocation: 'Sea-Tac Airport',
+  minCustPerHour: 3,
+  maxCustPerHour: 24,
+  avgCookiesSoldPerCust: 1.2,
+  customersPerHour: [],
+  cookiesPerHour: [],
+  hoursOpen: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
+  totalCookiesPerDay: 0,
+  generateRandomCustPerHour: function(min, max) {
+    for(var i = 0; i < this.hoursOpen.length; i++) {
+      var randomCustNumber = Math.floor(Math.random() * (max - min + 1) + min);
+      this.customersPerHour.push(randomCustNumber);
+    }
+  },
+  generateHourlySales: function() {
+    // Will generate number of customers per hour using the min/cust and max/cust
+    this.generateRandomCustPerHour(this.minCustPerHour, this.maxCustPerHour);
+
+    // loop to create and push a cookies per hour amount to pair with each hour open
+    for(var i = 0; i < this.hoursOpen.length; i++) {
+      var perHour = Math.round(this.customersPerHour[i] * this.avgCookiesSoldPerCust);
+      this.cookiesPerHour.push(perHour);
+
+      // creates a total amount of cookies for the whole day by adding up each cookies per hour amount
+      this.totalCookiesPerDay += perHour;
+    }
+  },
+  render: function() {
+    // Line below will generate hourly sales, which also generates customers per hour
+    this.generateHourlySales();
+  
+    var mainEl = document.getElementById('main-content');
+    var containerEl = document.createElement('section');
+  
+    var headingEl = document.createElement('h3');
+    headingEl.textContent = this.storeLocation;
+    containerEl.appendChild(headingEl);
+  
+    var ulEl = document.createElement('ul');
+  
+    for(var i = 0; i < this.hoursOpen.length; i++) {
+      var liEl = document.createElement('li');
+      liEl.textContent = `${this.hoursOpen[i]}: ${this.cookiesPerHour[i]} cookies`; // '6am: 23 cookies'
+      ulEl.appendChild(liEl);
+    }
+  
+    var totalEl = document.createElement('li');
+    totalEl.textContent = `Total: ${this.totalCookiesPerDay} cookies`;
+    ulEl.appendChild(totalEl);
+  
+    containerEl.appendChild(ulEl);
+    mainEl.appendChild(containerEl);
+  },
+};
+
+var seattleCenter = {
+  storeLocation: 'Seattle Center',
+  minCustPerHour: 11,
+  maxCustPerHour: 38,
+  avgCookiesSoldPerCust: 3.7,
+  customersPerHour: [],
+  cookiesPerHour: [],
+  hoursOpen: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
+  totalCookiesPerDay: 0,
+  generateRandomCustPerHour: function(min, max) {
+    for(var i = 0; i < this.hoursOpen.length; i++) {
+      var randomCustNumber = Math.floor(Math.random() * (max - min + 1) + min);
+      this.customersPerHour.push(randomCustNumber);
+    }
+  },
+  generateHourlySales: function() {
+    // Will generate number of customers per hour using the min/cust and max/cust
+    this.generateRandomCustPerHour(this.minCustPerHour, this.maxCustPerHour);
+
+    // loop to create and push a cookies per hour amount to pair with each hour open
+    for(var i = 0; i < this.hoursOpen.length; i++) {
+      var perHour = Math.round(this.customersPerHour[i] * this.avgCookiesSoldPerCust);
+      this.cookiesPerHour.push(perHour);
+
+      // creates a total amount of cookies for the whole day by adding up each cookies per hour amount
+      this.totalCookiesPerDay += perHour;
+    }
+  },
+  render: function() {
+    // Line below will generate hourly sales, which also generates customers per hour
+    this.generateHourlySales();
+  
+    var mainEl = document.getElementById('main-content');
+    var containerEl = document.createElement('section');
+  
+    var headingEl = document.createElement('h3');
+    headingEl.textContent = this.storeLocation;
+    containerEl.appendChild(headingEl);
+  
+    var ulEl = document.createElement('ul');
+  
+    for(var i = 0; i < this.hoursOpen.length; i++) {
+      var liEl = document.createElement('li');
+      liEl.textContent = `${this.hoursOpen[i]}: ${this.cookiesPerHour[i]} cookies`; // '6am: 23 cookies'
+      ulEl.appendChild(liEl);
+    }
+  
+    var totalEl = document.createElement('li');
+    totalEl.textContent = `Total: ${this.totalCookiesPerDay} cookies`;
+    ulEl.appendChild(totalEl);
+  
+    containerEl.appendChild(ulEl);
+    mainEl.appendChild(containerEl);
+  },
+};
+
+var capitolHill = {
+  storeLocation: 'Capitol Hill',
+  minCustPerHour: 20,
+  maxCustPerHour: 38,
+  avgCookiesSoldPerCust: 2.3,
+  customersPerHour: [],
+  cookiesPerHour: [],
+  hoursOpen: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
+  totalCookiesPerDay: 0,
+  generateRandomCustPerHour: function(min, max) {
+    for(var i = 0; i < this.hoursOpen.length; i++) {
+      var randomCustNumber = Math.floor(Math.random() * (max - min + 1) + min);
+      this.customersPerHour.push(randomCustNumber);
+    }
+  },
+  generateHourlySales: function() {
+    // Will generate number of customers per hour using the min/cust and max/cust
+    this.generateRandomCustPerHour(this.minCustPerHour, this.maxCustPerHour);
+
+    // loop to create and push a cookies per hour amount to pair with each hour open
+    for(var i = 0; i < this.hoursOpen.length; i++) {
+      var perHour = Math.round(this.customersPerHour[i] * this.avgCookiesSoldPerCust);
+      this.cookiesPerHour.push(perHour);
+
+      // creates a total amount of cookies for the whole day by adding up each cookies per hour amount
+      this.totalCookiesPerDay += perHour;
+    }
+  },
+  render: function() {
+    // Line below will generate hourly sales, which also generates customers per hour
+    this.generateHourlySales();
+  
+    var mainEl = document.getElementById('main-content');
+    var containerEl = document.createElement('section');
+  
+    var headingEl = document.createElement('h3');
+    headingEl.textContent = this.storeLocation;
+    containerEl.appendChild(headingEl);
+  
+    var ulEl = document.createElement('ul');
+  
+    for(var i = 0; i < this.hoursOpen.length; i++) {
+      var liEl = document.createElement('li');
+      liEl.textContent = `${this.hoursOpen[i]}: ${this.cookiesPerHour[i]} cookies`; // '6am: 23 cookies'
+      ulEl.appendChild(liEl);
+    }
+  
+    var totalEl = document.createElement('li');
+    totalEl.textContent = `Total: ${this.totalCookiesPerDay} cookies`;
+    ulEl.appendChild(totalEl);
+  
+    containerEl.appendChild(ulEl);
+    mainEl.appendChild(containerEl);
+  },
+};
+
+var alki = {
+  storeLocation: 'Alki',
+  minCustPerHour: 2,
+  maxCustPerHour: 16,
+  avgCookiesSoldPerCust: 4.6,
+  customersPerHour: [],
+  cookiesPerHour: [],
+  hoursOpen: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
+  totalCookiesPerDay: 0,
+  generateRandomCustPerHour: function(min, max) {
+    for(var i = 0; i < this.hoursOpen.length; i++) {
+      var randomCustNumber = Math.floor(Math.random() * (max - min + 1) + min);
+      this.customersPerHour.push(randomCustNumber);
+    }
+  },
+  generateHourlySales: function() {
+    // Will generate number of customers per hour using the min/cust and max/cust
+    this.generateRandomCustPerHour(this.minCustPerHour, this.maxCustPerHour);
+
+    // loop to create and push a cookies per hour amount to pair with each hour open
+    for(var i = 0; i < this.hoursOpen.length; i++) {
+      var perHour = Math.round(this.customersPerHour[i] * this.avgCookiesSoldPerCust);
+      this.cookiesPerHour.push(perHour);
+
+      // creates a total amount of cookies for the whole day by adding up each cookies per hour amount
+      this.totalCookiesPerDay += perHour;
+    }
+  },
+  render: function() {
+    // Line below will generate hourly sales, which also generates customers per hour
+    this.generateHourlySales();
+  
+    var mainEl = document.getElementById('main-content');
+    var containerEl = document.createElement('section');
+  
+    var headingEl = document.createElement('h3');
+    headingEl.textContent = this.storeLocation;
+    containerEl.appendChild(headingEl);
+  
+    var ulEl = document.createElement('ul');
+  
+    for(var i = 0; i < this.hoursOpen.length; i++) {
+      var liEl = document.createElement('li');
+      liEl.textContent = `${this.hoursOpen[i]}: ${this.cookiesPerHour[i]} cookies`; // '6am: 23 cookies'
+      ulEl.appendChild(liEl);
+    }
+  
+    var totalEl = document.createElement('li');
+    totalEl.textContent = `Total: ${this.totalCookiesPerDay} cookies`;
+    ulEl.appendChild(totalEl);
+  
+    containerEl.appendChild(ulEl);
+    mainEl.appendChild(containerEl);
+  },
+};
+
+firstAndPike.render();
+seatacAirport.render();
+seattleCenter.render();
+capitolHill.render();
+alki.render();
